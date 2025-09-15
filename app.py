d2204534d45c234c0f1866609c000511068c7397
Connor Maxwell <connor.maxwell@teamfeepay.com>
2025-09-15 14:58:27 +0100

    Initial commit

--- app.py
+++ app.py
@@ -170,6 +170,6 @@ def download_file(filename):
 # Run
 # =========================
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)
+
